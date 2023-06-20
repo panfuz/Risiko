747c4e068b5bef056950d38e7a1e317b2d6eb0e3
panfuz <126594404+panfuz@users.noreply.github.com>
2023-06-20 14:18:29 +0200

    xx
    x

---
+++ Obiettivo.cs
@@ -0,0 +1,11 @@
+public class Obiettivo
+{
+    string id;
+    string descrizione;
+
+    public Obiettivo(string Id, string Descrizione)
+    {
+        id = Id;
+        descrizione = Descrizione;
+    }
+}
