68aa2d5de40a7677c5fba7b8875d2268c6e771bb
panfuz <126594404+panfuz@users.noreply.github.com>
2023-06-20 15:06:10 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,1 @@
-public class Truppa
-{
-    string id { get; set; }
-    string id_esercito { get; set; }
 
-    public Truppa(string Id, string Id_esercito)
-    {
-        id = Id;
-        id_esercito = Id_esercito;
-    }
-}
