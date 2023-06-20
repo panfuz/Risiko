6701c344975149986d18b23ddec9fd1b50885a82
panfuz <126594404+panfuz@users.noreply.github.com>
2023-06-20 15:38:43 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿
+Obiettivo o1 = new Obiettivo("o1", "Conquista la totalità di Asia e America del Sud");
+Obiettivo o2 = new Obiettivo("o2", "Conquista la totalità di Oceania e Nord America");
+Obiettivo o3 = new Obiettivo("o3", "Conquista la totalità di Africa, Europa e un continente a scelta");
+Obiettivo o4 = new Obiettivo("o4", "Annienta le armate rosse, se sei tu o vengono annientate da un altro giocatore conquista 24 territori");
+Obiettivo o5 = new Obiettivo("o5", "Annienta le armate nere, se sei tu o vengono annientate da un altro giocatore conquista 24 territori");
+Obiettivo o6 = new Obiettivo("o6", "Conquista la totalità di Oceania, Asia e America del Sud");﻿
 
