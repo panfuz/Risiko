f650a1bc74263a2ad760acf29a3ec2b2739aa13c
panfuz <126594404+panfuz@users.noreply.github.com>
2023-06-20 15:05:32 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿pluto
+public class Truppa
+{
+    string id { get; set; }
+    string id_esercito { get; set; }
+
+    public Truppa(string Id, string Id_esercito)
+    {
+        id = Id;
+        id_esercito = Id_esercito;
+    }
+}
