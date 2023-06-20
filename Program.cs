3ddd2424a8ff0d0c1f9395d15fd7062a09c17b6c
Andrea <126594542+xPANFILI@users.noreply.github.com>
2023-06-20 16:26:23 +0200

    yt
    yt

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
-﻿Territorio american1 = new Territorio("american1", "Groenlandia", "c1", "cavaliere", confini[]);
+﻿
+Territorio american1 = new Territorio("american1", "Groenlandia", "c1", "cavaliere", confini[]);
 Territorio american2 = new Territorio("american2", "Alaska", "american", "c1", confini[]);
 Territorio american3 = new Territorio("american3", "Territori Nord-Ovest", "c1", "cavaliere", confini[]);
 Territorio american4 = new Territorio("american4", "Alberta", "c1", "cavaliere", confini[]);
@@ -117,14 +117,14 @@ List<Territorio> oceania = new List<Territorio>() {
 
 
 
-=======
-Continente c1 = new Continente("c1", "Nord America", new List<Territorio>() {americaNord });
-Continente c2 = new Continente("c2", "Sud America", new List<Territorio>() {americaSud });
-Continente c3 = new Continente("c3", "Africa", new List<Territorio>() {africa });
-Continente c4 = new Continente("c4", "Europa", new List<Territorio>() {europa });
-Continente c5 = new Continente("c5", "Asia", new List<Territorio>() {asia });
-Continente c6 = new Continente("c6", "Oceania", new List<Territorio>() {oceania });
->>>>>>> b86c76702596be4c3a7b312d3585b6ed975ce8fb
+
+Continente c1 = new Continente("c1", "Nord America", americaNord);
+Continente c2 = new Continente("c2", "Sud America", americaSud);
+Continente c3 = new Continente("c3", "Africa", africa);
+Continente c4 = new Continente("c4", "Europa", new List<Territorio>() { europa });
+Continente c5 = new Continente("c5", "Asia", new List<Territorio>() { asia });
+Continente c6 = new Continente("c6", "Oceania", new List<Territorio>() { oceania });
+
 
 Obiettivo o1 = new Obiettivo("o1", "Conquista la totalità di Asia e America del Sud");
 Obiettivo o2 = new Obiettivo("o2", "Conquista la totalità di Oceania e Nord America");
